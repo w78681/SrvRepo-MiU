531e29ef17a7f982f5e805f194876c325fa2c3bc
Jeremy Sweet <Jeremys@fullsail.edu>
2013-07-25 08:01:06 -0400

    Updated all files.
    Updated all files.

--- js/Sample-main.js
+++ js/Sample-main.js
@@ -1,10 +1,10 @@
-$('#home').on('pageinit', function(){
+$('#pageMain').on('pageinit', function(){
 	//code needed for home page goes here
 });	
 		
-$('#addItem').on('pageinit', function(){
+$('#pageAddItemForm').on('pageinit', function(){
 
-		var myForm = $('#formId');
+		var myForm = $('#addItemForm');
 		    myForm.validate({
 			invalidHandler: function(form, validator) {
 			},
@@ -20,6 +20,14 @@ $('#addItem').on('pageinit', function(){
 
 //The functions below can go inside or outside the pageinit function for the page in which it is needed.
 
+function checkBoxValue(){
+	if (getelement('itemspoil').checked){
+		spoilValue = getelement('itemspoil').value;
+	} else {
+		spoilValue = "No";
+	}
+}
+
 var autofillData = function (){
 	 
 };
@@ -29,7 +37,24 @@ var getData = function(){
 };
 
 var storeData = function(data){
-	
+	//if there is no key, this if statement sets a new key and stores data
+	if (!key){
+		var id = Math.floor(Math.random()*102363265439);
+	}else{
+		id = key;
+	}
+	checkBoxValue();
+	var item				= {};
+		item.name			= ["Name: ", getelement('itemName').value];
+		item.category		= ["Category: ", getelement('itemType').value];
+		item.cost			= ["Cost: ", getelement('itemCost').value];
+		item.ammount		= ["Ammount: ", getelement('itemAmount').value];
+		item.spoil			= ["Spoil: ", spoilValue];
+		item.removedate 	= ["Remove Date: ", getelement('itemRemoveDate').value];
+		item.description	= ["Description: ", getelement('itemDescription').value];
+		
+		localStorage.setItem(id, JSON.stringify(item));
+		alert("Data Saved!");
 }; 
 
 var	deleteItem = function (){
@@ -40,4 +65,5 @@ var clearLocal = function(){
 
 };
 
+var spoilValue = "No";
 
